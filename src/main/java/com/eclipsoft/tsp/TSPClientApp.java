d9a73d4116665e65011f005ed788f523d32b3ba5
Gabriel Villacis <gvillacis@eclipsoft.com>
2021-12-29 17:53:06 -0500

    Add logback logger

---
+++ src/main/java/com/eclipsoft/tsp/TSPClientApp.java
@@ -0,0 +1,13 @@
+package com.eclipsoft.tsp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TSPClientApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(TSPClientApp.class);
+
+    public static void main(String[] args) {
+    }
+
+}
